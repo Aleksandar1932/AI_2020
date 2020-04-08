91ae90f18cf5a884bde733ae462cb30760c7d721
aleksandar <aleksandar.ivanovski123@gmail.com>
2020-04-08 23:41:33 +0200

    Sanke Informed and Uninformed search Solve Snake_1 and Snake_2

--- exercises/informed_search/snake.py
+++ exercises/informed_search/snake.py
@@ -1,148 +1,146 @@
-import sys
+# from searching_framework.informed_search import *
+# from searching_framework.utils import *
+from sys import maxsize as infinity
 
-
-def tree_search(problem, fringe):
-    """ Пребарувај низ следбениците на даден проблем за да најдеш цел.
-    :param problem: даден проблем
-    :type problem: Problem
-    :param fringe:  празна редица (queue)
-    :type fringe: FIFOQueue or Stack or PriorityQueue
-    :return: Node or None
-    :rtype: Node
-    """
-    fringe.append(Node(problem.initial))
-    while fringe:
-        node = fringe.pop()
-        print(node.state)
-        if problem.goal_test(node.state):
-            return node
-        fringe.extend(node.expand(problem))
-    return None
-
-
-def breadth_first_tree_search(problem):
-    """Експандирај го прво најплиткиот јазол во пребарувачкото дрво.
-    :param problem: даден проблем
-    :type problem: Problem
-    :return: Node or None
-    :rtype: Node
-    """
-    return tree_search(problem, FIFOQueue())
-
-
-def depth_first_tree_search(problem):
-    """Експандирај го прво најдлабокиот јазол во пребарувачкото дрво.
-    :param problem: даден проблем
-    :type problem: Problem
-    :return: Node or None
-    :rtype: Node
-    """
-    return tree_search(problem, Stack())
-
+# from .utils import Node, PriorityQueue
 
 """
-Неинформирано пребарување во рамки на граф
-Основната разлика е во тоа што овде не дозволуваме јамки, 
-т.е. повторување на состојби
+Информирано пребарување во рамки на граф
 """
 
 
-def graph_search(problem, fringe):
-    """Пребарувај низ следбениците на даден проблем за да најдеш цел.
-     Ако до дадена состојба стигнат два пата, употреби го најдобриот пат.
+def memoize(fn, slot=None):
+    """ Запамети ја пресметаната вредност за која била листа од
+    аргументи. Ако е специфициран slot, зачувај го резултатот во
+    тој slot на првиот аргумент. Ако slot е None, зачувај ги
+    резултатите во речник.
+
+    :param fn: зададена функција
+    :type fn: function
+    :param slot: име на атрибут во кој се чуваат резултатите од функцијата
+    :type slot: str
+    :return: функција со модификација за зачувување на резултатите
+    :rtype: function
+    """
+    if slot:
+        def memoized_fn(obj, *args):
+            if hasattr(obj, slot):
+                return getattr(obj, slot)
+            else:
+                val = fn(obj, *args)
+                setattr(obj, slot, val)
+                return val
+    else:
+        def memoized_fn(*args):
+            if args not in memoized_fn.cache:
+                memoized_fn.cache[args] = fn(*args)
+            return memoized_fn.cache[args]
+
+        memoized_fn.cache = {}
+    return memoized_fn
+
+
+def best_first_graph_search(problem, f):
+    """Пребарувај низ следбениците на даден проблем за да најдеш цел. Користи
+     функција за евалуација за да се одлучи кој е сосед најмногу ветува и
+     потоа да се истражи. Ако до дадена состојба стигнат два пата, употреби
+     го најдобриот пат.
+
     :param problem: даден проблем
     :type problem: Problem
-    :param fringe:  празна редица (queue)
-    :type fringe: FIFOQueue or Stack or PriorityQueue
+    :param f: дадена функција за евалуација (проценка)
+    :type f: function
     :return: Node or None
     :rtype: Node
     """
-    closed = set()
-    fringe.append(Node(problem.initial))
-    while fringe:
-        node = fringe.pop()
+    f = memoize(f, 'f')
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = PriorityQueue(min, f)
+    frontier.append(node)
+    explored = set()
+    while frontier:
+        node = frontier.pop()
         if problem.goal_test(node.state):
             return node
-        if node.state not in closed:
-            closed.add(node.state)
-            fringe.extend(node.expand(problem))
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
+            elif child in frontier:
+                incumbent = frontier[child]
+                if f(child) < f(incumbent):
+                    del frontier[incumbent]
+                    frontier.append(child)
     return None
 
 
-def breadth_first_graph_search(problem):
-    """Експандирај го прво најплиткиот јазол во пребарувачкиот граф.
+def greedy_best_first_graph_search(problem, h=None):
+    """ Greedy best-first пребарување се остварува ако се специфицира дека f(n) = h(n).
+
     :param problem: даден проблем
     :type problem: Problem
+    :param h: дадена функција за хевристика
+    :type h: function
     :return: Node or None
-    :rtype: Node
     """
-    return graph_search(problem, FIFOQueue())
+    h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, h)
 
 
-def depth_first_graph_search(problem):
-    """Експандирај го прво најдлабокиот јазол во пребарувачкиот граф.
+def astar_search(problem, h=None):
+    """ A* пребарување е best-first graph пребарување каде f(n) = g(n) + h(n).
+
     :param problem: даден проблем
     :type problem: Problem
+    :param h: дадена функција за хевристика
+    :type h: function
     :return: Node or None
-    :rtype: Node
     """
-    return graph_search(problem, Stack())
+    h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
 
 
-def depth_limited_search(problem, limit=50):
-    """Експандирај го прво најдлабокиот јазол во пребарувачкиот граф
-    со ограничена длабочина.
+def recursive_best_first_search(problem, h=None):
+    """Recursive best first search - ја ограничува рекурзијата
+    преку следење на f-вредноста на најдобриот алтернативен пат
+    од било кој јазел предок (еден чекор гледање нанапред).
+
     :param problem: даден проблем
     :type problem: Problem
-    :param limit: лимит за длабочината
-    :type limit: int
+    :param h: дадена функција за хевристика
+    :type h: function
     :return: Node or None
-    :rtype: Node
     """
+    h = memoize(h or problem.h, 'h')
 
-    def recursive_dls(node, problem, limit):
-        """Помошна функција за depth limited"""
-        cutoff_occurred = False
+    def RBFS(problem, node, flimit):
         if problem.goal_test(node.state):
-            return node
-        elif node.depth == limit:
-            return 'cutoff'
-        else:
-            for successor in node.expand(problem):
-                result = recursive_dls(successor, problem, limit)
-                if result == 'cutoff':
-                    cutoff_occurred = True
-                elif result is not None:
-                    return result
-        if cutoff_occurred:
-            return 'cutoff'
-        return None
+            return node, 0  # (втората вредност е неважна)
+        successors = node.expand(problem)
+        if len(successors) == 0:
+            return None, infinity
+        for s in successors:
+            s.f = max(s.path_cost + h(s), node.f)
+        while True:
+            # Подреди ги според најниската f вредност
+            successors.sort(key=lambda x: x.f)
+            best = successors[0]
+            if best.f > flimit:
+                return None, best.f
+            if len(successors) > 1:
+                alternative = successors[1].f
+            else:
+                alternative = infinity
+            result, best.f = RBFS(problem, best, min(flimit, alternative))
+            if result is not None:
+                return result, best.f
 
-    return recursive_dls(Node(problem.initial), problem, limit)
-
-
-def iterative_deepening_search(problem):
-    """Експандирај го прво најдлабокиот јазол во пребарувачкиот граф
-    со ограничена длабочина, со итеративно зголемување на длабочината.
-    :param problem: даден проблем
-    :type problem: Problem
-    :return: Node or None
-    :rtype: Node
-    """
-    for depth in range(sys.maxsize):
-        result = depth_limited_search(problem, depth)
-        if result is not 'cutoff':
-            return result
-
-
-def uniform_cost_search(problem):
-    """Експандирај го прво јазолот со најниска цена во пребарувачкиот граф.
-    :param problem: даден проблем
-    :type problem: Problem
-    :return: Node or None
-    :rtype: Node
-    """
-    return graph_search(problem, PriorityQueue(min, lambda a: a.path_cost))
+    node = Node(problem.initial)
+    node.f = h(node)
+    result, bestf = RBFS(problem, node, infinity)
+    return result
 
 
 import bisect
@@ -209,7 +207,7 @@ class Problem:
         state2 од состојбата state1 преку акцијата action, претпоставувајќи
         дека цената на патот до состојбата state1 е c. Ако проблемот е таков
         што патот не е важен, оваа функција ќе ја разгледува само состојбата
-        state2. Ако патот е важен, ќе ја разгледува цената c и можеби и
+        state2. Ако патот е важен, ќе ја разгледува цената c и можеби и
         state1 и action. Даденава имплементација му доделува цена 1 на секој
         чекор од патот.
 
@@ -223,7 +221,7 @@ class Problem:
         return c + 1
 
     def value(self):
-        """За проблеми на оптимизација, секоја состојба си има вредност.
+        """За проблеми на оптимизација, секоја состојба си има вредност. 
         Hill-climbing и сличните алгоритми се обидуваат да ја максимизираат
         оваа вредност.
 
@@ -335,11 +333,11 @@ class Node:
 
 class Queue:
     """Queue е апстрактна класа / интерфејс. Постојат 3 типа:
-        Stack(): Last In First Out Queue (стек).
-        FIFOQueue(): First In First Out Queue (редица).
-        PriorityQueue(order, f): Queue во сортиран редослед (подразбирливо,од најмалиот кон
+        Stack(): Last In First Out Queue (стек).
+        FIFOQueue(): First In First Out Queue (редица).
+        PriorityQueue(order, f): Queue во сортиран редослед (подразбирливо,од најмалиот кон
                                  најголемиот јазол).
-    """
+    """
 
     def __init__(self):
         raise NotImplementedError
@@ -476,20 +474,21 @@ class PriorityQueue(Queue):
                 self.data.pop(i)
 
 
-def move_down(snake, green, red):
-    snake_list = list(
-        snake)  # nikogash ne pravi prefrlanje na cela lista i site elementi vo torka i obratno, zahtevna operacija e
+#
+
+from statistics import mean
+
+def move_down(snake, green):
+    snake_list = list(snake)
     green_list = list(green)
-    red_list = list(red)
 
     # y - 1
     curr_head = snake_list[-1]
     curr_x = curr_head[0]
     curr_y = curr_head[1]
 
-    if curr_y - 1 >= 0 and (curr_x, curr_y - 1) not in red_list and (
-            curr_x, curr_y - 1) not in snake_list:  # mesto da proveruvash dali sodrzi lista element proveri si torka
-        new_head = (curr_x, curr_y - 1)  # faleshe proverka dali zmijata si jade del od teloto
+    if curr_y - 1 >= 0 and (curr_x, curr_y - 1) not in snake_list:
+        new_head = (curr_x, curr_y - 1)
         if new_head in green_list:
             green_list.remove(new_head)
             snake_list.append(new_head)
@@ -500,17 +499,16 @@ def move_down(snake, green, red):
     return tuple(snake_list), tuple(green_list)
 
 
-def move_up(snake, green, red):
+def move_up(snake, green):
     snake_list = list(snake)
     green_list = list(green)
-    red_list = list(red)
 
     # y - 1
     curr_head = snake_list[-1]
     curr_x = curr_head[0]
     curr_y = curr_head[1]
 
-    if curr_y + 1 < 10 and (curr_x, curr_y + 1) not in red_list and (curr_x, curr_y + 1) not in snake_list:
+    if curr_y + 1 < 10 and (curr_x, curr_y + 1) not in snake_list:
         new_head = (curr_x, curr_y + 1)
         if new_head in green_list:
             green_list.remove(new_head)
@@ -522,17 +520,16 @@ def move_up(snake, green, red):
     return tuple(snake_list), tuple(green_list)
 
 
-def move_right(snake, green, red):
+def move_right(snake, green):
     snake_list = list(snake)
     green_list = list(green)
-    red_list = list(red)
 
     # x + 1
     curr_head = snake_list[-1]
     curr_x = curr_head[0]
     curr_y = curr_head[1]
 
-    if curr_x + 1 < 10 and (curr_x + 1, curr_y) not in red_list and (curr_x + 1, curr_y) not in snake_list:
+    if curr_x + 1 < 10 and (curr_x + 1, curr_y) not in snake_list:
         new_head = (curr_x + 1, curr_y)
         if new_head in green_list:
             green_list.remove(new_head)
@@ -544,17 +541,16 @@ def move_right(snake, green, red):
     return tuple(snake_list), tuple(green_list)
 
 
-def move_left(snake, green, red):
+def move_left(snake, green):
     snake_list = list(snake)
     green_list = list(green)
-    red_list = list(red)
 
     # x - 1
     curr_head = snake_list[-1]
     curr_x = curr_head[0]
     curr_y = curr_head[1]
 
-    if curr_x - 1 >= 0 and (curr_x - 1, curr_y) not in red_list and (curr_x - 1, curr_y) not in snake_list:
+    if curr_x - 1 >= 0 and (curr_x - 1, curr_y) not in snake_list:
         new_head = (curr_x - 1, curr_y)
         if new_head in green_list:
             green_list.remove(new_head)
@@ -568,13 +564,11 @@ def move_left(snake, green, red):
 
 class Snake(Problem):
 
-    def __init__(self, initial, red, goal=None):
+    def __init__(self, initial, goal=None):
         super().__init__(initial, goal)
-        self.red = red
 
     def successor(self, state):
         successor = {}
-        red = self.red
         curr_snake = state[0]
         curr_direction = state[1]
         curr_green = state[2]
@@ -582,50 +576,50 @@ class Snake(Problem):
         # ProdolzhiPravo:
         if curr_direction == "S":
             new_direction = curr_direction
-            new_snake, new_green = move_down(curr_snake, curr_green, red)
+            new_snake, new_green = move_down(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["ProdolzhiPravo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "N":
             new_direction = curr_direction
-            new_snake, new_green = move_up(curr_snake, curr_green, red)
+            new_snake, new_green = move_up(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["ProdolzhiPravo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "E":
             new_direction = curr_direction
-            new_snake, new_green = move_right(curr_snake, curr_green, red)
+            new_snake, new_green = move_right(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["ProdolzhiPravo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "W":
             new_direction = curr_direction
-            new_snake, new_green = move_left(curr_snake, curr_green, red)
+            new_snake, new_green = move_left(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["ProdolzhiPravo"] = (new_snake, new_direction, new_green)
 
         # SvrtiDesno
         if curr_direction == "S":
             new_direction = "W"
-            new_snake, new_green = move_left(curr_snake, curr_green, red)
+            new_snake, new_green = move_left(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiDesno"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "N":
             new_direction = "E"
-            new_snake, new_green = move_right(curr_snake, curr_green, red)
+            new_snake, new_green = move_right(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiDesno"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "E":
             new_direction = "S"
-            new_snake, new_green = move_down(curr_snake, curr_green, red)
+            new_snake, new_green = move_down(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiDesno"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "W":
             new_direction = "N"
-            new_snake, new_green = move_up(curr_snake, curr_green, red)
+            new_snake, new_green = move_up(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiDesno"] = (new_snake, new_direction, new_green)
 
@@ -633,25 +627,25 @@ class Snake(Problem):
 
         if curr_direction == "S":
             new_direction = "E"
-            new_snake, new_green = move_right(curr_snake, curr_green, red)
+            new_snake, new_green = move_right(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiLevo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "N":
             new_direction = "W"
-            new_snake, new_green = move_left(curr_snake, curr_green, red)
+            new_snake, new_green = move_left(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiLevo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "E":
             new_direction = "N"
-            new_snake, new_green = move_up(curr_snake, curr_green, red)
+            new_snake, new_green = move_up(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiLevo"] = (new_snake, new_direction, new_green)
 
         elif curr_direction == "W":
             new_direction = "S"
-            new_snake, new_green = move_down(curr_snake, curr_green, red)
+            new_snake, new_green = move_down(curr_snake, curr_green)
             if new_snake != curr_snake:
                 successor["SvrtiLevo"] = (new_snake, new_direction, new_green)
 
@@ -666,6 +660,21 @@ class Snake(Problem):
     def goal_test(self, state):
         return len(state[2]) == 0
 
+    def h(self, node):
+        # total_mhd = 0
+        # if len(node.state[2]) != 0:
+        #     for apple_coord in node.state[2]:
+        #         total_mhd += mhd(node.state[0][-1], apple_coord)
+        #     return total_mhd / len(node.state[2])
+        #
+        # return total_mhd
+        if len(node.state[2]) != 0:
+            return mean(([mhd(node.state[0][-1], el) for el in node.state[2]]))
+        return 0
+
+def mhd(p1, p2):
+    return abs(p1[0] - p2[0]) - abs(p1[1] - p2[1])
+
 
 if __name__ == '__main__':
     snake_initial = ((0, 9), (0, 8), (0, 7))
@@ -673,10 +682,6 @@ if __name__ == '__main__':
 
     n = int(input())
     green = tuple(tuple(map(int, input().split(','))) for _ in range(n))
-    m = int(input())
-    red = tuple(tuple(map(int, input().split(','))) for _ in range(m))
 
-    snake_problem = Snake((snake_initial, snake_direction, green), red)
-    print(breadth_first_graph_search(snake_problem).solution())
-
-    # ne treba da pravish main metoda i da ja povikuvash vo main metodata
+    snake_problem = Snake((snake_initial, snake_direction, green))
+    print(astar_search(snake_problem).solution())
